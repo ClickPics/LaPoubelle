cf90ec95bc2da665f4116eff16dfcf388a183b71
Clint <cougarclint@hotmail.com>
2017-09-26 18:48:05 -0700

    data library setup

---
+++ ZenithDataLib/Models/ActivityCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenithDataLib
+{
+    public class ActivityCategory
+    {
+        public int ActivityCategoryId { get; set; }
+
+        public string ActivityDescription { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
