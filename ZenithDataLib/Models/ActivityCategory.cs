09f5d94fb821bdd6e00d12a0944417c503b726bb
Matthew Li <li.matthew.m@gmail.com>
2017-10-13 03:00:58 -0700

    Added event seed, added Data annotations for display, Removed unnecessary task bar item, set page titles

--- ZenithDataLib/Models/ActivityCategory.cs
+++ ZenithDataLib/Models/ActivityCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,10 @@ namespace ZenithDataLib.Models
         
         public int ActivityCategoryId { get; set; }
 
+        [Display(Name = "Activity Description")]
         public string ActivityDescription { get; set; }
 
+        [Display(Name = "Created On")]
         public DateTime CreationDate { get; set; }
     }
     
--- ZenithDataLib/Models/Event.cs
+++ ZenithDataLib/Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,16 @@ namespace ZenithDataLib.Models
     {
         public int EventId { get; set; }
 
+        [Display(Name = "Start Time")]
         public DateTime StartDateTime { get; set; }
 
+        [Display(Name = "End Time")]
         public DateTime EndDateTime { get; set; }
 
+        [Display(Name = "Created By")]
         public string Username { get; set; }
 
+        [Display(Name = "Created On")]
         public DateTime CreationDate { get; set; }
 
         public bool IsActive { get; set; }
@@ -27,6 +32,7 @@ namespace ZenithDataLib.Models
         [ForeignKey("ActivityCategory")]
         public int ActivityCategoryId { get; set; }
 
+        [Display(Name = "Activity Description")]
         public ActivityCategory ActivityCategory { get; set; }
     }
 }
